d8c19655caa7c96b9b59068f4b1d819ee0d4a9cb
piatus <marcincotoejst@wp.pl>
2013-06-06 14:52:24 +0200

    Update make.sh
    Dodanie skryptu kompilacji w środowisku windowsa.

--- xyzzydm/gamemode/gamemodes/make.sh
+++ xyzzydm/gamemode/gamemodes/make.sh
@@ -16,3 +16,19 @@ echo \#define GMPORT $GMPORT >> ../include/fullserver/version.inc
 WINEPREFIX=/vol/n/.wg/ wine /pawno/pawncc -i/pawno/include_0.3c -i..\\include fs.pwn  -\;\+ -\\ -\(\+ && ncftpput -u login -p haslo -C 91.204.162.80 fs.amx gamemodes/fs.amx
 #WINEPREFIX=/vol/n/.wg/ wine /pawno/pawncc -i/pawno/include_0.3c -i..\\include fs.pwn  -\;\+ -\\ -\(\+ && scp -P 57022 fs.amx pio@fsadm.i64.pl:~/fs/samp03c/gamemodes/
 
+#Windows wersion compilation of gamemode
+                  #by mrdrifter
+Copy and save file *.bat 
+@echo off
+echo \#define GMVERSION \"3.3r$SVER\" > .././pawno/include/fullserver/version.inc
+set KIEDY=`date +%x\ %T`
+set GDZIE=`hostname`
+set GMHOST="10.39.7.2"
+set GMPORT="8888"
+
+echo \#define GMCOMPILED \"skompilowana $KIEDY przez $USER@$GDZIE\" >> .././include/fullserver/version.inc
+echo \#define GMHOST \"$GMHOST\" >> .././include/fullserver/version.inc
+echo \#define GMPORT $GMPORT >> .././include/fullserver/version.inc
+
+".././pawno/pawncc.exe" "fs.pwn" -; -( -r
+pause
