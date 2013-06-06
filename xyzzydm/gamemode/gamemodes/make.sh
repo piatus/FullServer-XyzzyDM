87e93a5aa99816d34bbbd4e5bbf71976ae11be21
piatus <marcincotoejst@wp.pl>
2013-06-06 14:56:17 +0200

    Update make.sh

--- xyzzydm/gamemode/gamemodes/make.sh
+++ xyzzydm/gamemode/gamemodes/make.sh
@@ -1,3 +1,4 @@
+#if using compiler the windows to delete..
 #!/bin/sh
 
 date
@@ -15,6 +16,7 @@ echo \#define GMPORT $GMPORT >> ../include/fullserver/version.inc
 
 WINEPREFIX=/vol/n/.wg/ wine /pawno/pawncc -i/pawno/include_0.3c -i..\\include fs.pwn  -\;\+ -\\ -\(\+ && ncftpput -u login -p haslo -C 91.204.162.80 fs.amx gamemodes/fs.amx
 #WINEPREFIX=/vol/n/.wg/ wine /pawno/pawncc -i/pawno/include_0.3c -i..\\include fs.pwn  -\;\+ -\\ -\(\+ && scp -P 57022 fs.amx pio@fsadm.i64.pl:~/fs/samp03c/gamemodes/
+#if using compiler the windows to end delete
 
 #Windows wersion compilation of gamemode
                   #by mrdrifter
